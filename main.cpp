91098c5ca7393f467e61d19419848530e6457c15
mithila0611 <158758351+mithila0611@users.noreply.github.com>
2024-02-18 22:34:49 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    cout<<"Enter your name : ";
+    string name;
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    return 0;
+}
