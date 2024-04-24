3e3baafb0a32ccc120c52cc8c693f637a4002df5
mithila0611 <158758351+mithila0611@users.noreply.github.com>
2024-04-25 00:23:45 +0530

    Add files via upload

--- ExceptionHand.java
+++ ExceptionHand.java
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -6,27 +14,28 @@ import java.io.IOException;
 class ExceptionHand{
     public
     float result;
-    void divide(int num[]){
-        try{
-            for(int i=0;i<10;i++)
+        void divide(int num[]){
             try{
-                int a;
-                result=2/num[i];
-                System.out.println("the result= "+result);
+                for(int i=0;i<10;i++)
+                try{
+                    int a;
+                    result=2/num[i];
+                    System.out.println("the result= "+result);
+                }
+                catch(ArithmeticException obj){
+                    System.out.println("Third exception");
+                    System.out.println("Divide by zero is not possible send any integer type data");
+                }
             }
-            catch(ArithmeticException obj){
-                System.out.println("Third exception");
-                System.out.println("Divide by zero is not possible send any integer type data");
+            catch(IndexOutOfBoundsException obj){
+                System.out.println("Fourth exception");
+                System.out.println("The iteration exceeded its limts");
             }
-        }
-        catch(IndexOutOfBoundsException obj){
-            System.out.println("Fourth exception");
-            System.out.println("The iteration exceeded its limts");
-        }
-        catch(NullPointerException n){
-        }
-        finally{
-            System.out.println("Block of code is executed");
+            catch(NullPointerException n){
+                
+            }
+            finally{
+                System.out.println("Block of code is executed");
             }
         }
         public void create(){
@@ -42,32 +51,35 @@ class ExceptionHand{
                 }
             }
             catch(FileNotFoundException obj){
-            System.out.println("File you created is not available");
-        }   
+                System.out.println("File you created is not available");
+            }   
     
-    }   
-    public static void main(String args[]){
-        int num[]={1,2,0,4,5};
-        ExceptionHand obj;
-        try{
-            obj=new ExceptionHand();
-            obj.getClass().getMethod("Divide");
-        }
-        catch(NoSuchMethodException e){
-            System.out.println("first exception");
-            System.out.println("funtion is not available");
-        }
-        try{
-            String a=null;
-            System.out.println(a.length());
-        }
-        catch(NullPointerException e){
-            System.out.println("Second Exception:\nNull value is initiated");
-        }
-        finally{
-            obj=new ExceptionHand();
-            obj.divide(num);
-            obj.create();
-        }
+        }   
+        public static void main(String args[]){
+            int num[]={1,2,0,4,5};
+
+            ExceptionHand obj;
+            try{
+                obj=new ExceptionHand();
+                obj.getClass().getMethod("Divide");
+           
+            }
+            catch(NoSuchMethodException e){
+                System.out.println("first exception");
+                System.out.println("funtion is not available");
+       
+            }
+            try{
+                String a=null;
+                System.out.println(a.length());
+            }
+            catch(NullPointerException e){
+                System.out.println("Second Exception:\nNull value is initiated");
+            }
+            finally{
+                obj=new ExceptionHand();
+                obj.divide(num);
+                obj.create();
+            }
     }
 }
